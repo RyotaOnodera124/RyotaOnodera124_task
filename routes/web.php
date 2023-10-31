fc49ddb6174e3b996017f1ee6ebd3e1d579b53ef
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-10-31 12:09:08 +0900

    課題修正完了

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::resource('tasks', TaskController::class);
